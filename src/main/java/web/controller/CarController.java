0cd1cdf0fc46dbb1dcfa96343f75f70862064f5b
vepolud <vepolud@gmail.com>
2022-05-29 13:34:30 +0300

    созданы и работают болванки Car, car.html, carController

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,21 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+    @GetMapping(value = "/cars")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello Cars!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+}
---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,55 @@
+package web.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "prototype")
+public class Car {
+
+    String model;
+    String color;
+    int horsePower;
+
+    public Car() {
+    }
+
+    public Car(String model, String color, int horsePower) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", horsePower=" + horsePower +
+                '}';
+    }
+}
