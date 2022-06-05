2dceb1424cb3339e4e5aec9d49323a8ceb14b8ea
vepolud <vepolud@gmail.com>
2022-06-05 15:15:53 +0300

    Всё сделано

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,29 +1,20 @@
 package web.controller;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.config.WebConfig;
 import web.model.Car;
 import web.service.CarServiceImp;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
-//    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
-//    CarServiceImp carServiceImp = context.getBean("CarServiceImp", CarServiceImp.class);
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "5") int count, ModelMap model) {
-//        List<Car> messages = carServiceImp.getCarList(5);
         List<Car> messages = CarServiceImp.getCarList(count);
-//        messages.add("Hello Cars!");
-//        messages.add("I'm Spring MVC application");
-//        messages.add("5.2.0 version by sep'19 ");
         model.addAttribute("messages", messages);
-        return "index";
+        return "cars";
     }
 }
