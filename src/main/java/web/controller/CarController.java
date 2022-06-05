cba9c991baed0b41284dcf3ee8733be69a165860
vepolud <vepolud@gmail.com>
2022-06-05 15:06:53 +0300

    Всё сделано

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.config.WebConfig;
 import web.model.Car;
 import web.service.CarServiceImp;
@@ -16,9 +17,9 @@ public class CarController {
 //    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
 //    CarServiceImp carServiceImp = context.getBean("CarServiceImp", CarServiceImp.class);
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
+    public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "5") int count, ModelMap model) {
 //        List<Car> messages = carServiceImp.getCarList(5);
-        List<Car> messages = CarServiceImp.getCarList(5);
+        List<Car> messages = CarServiceImp.getCarList(count);
 //        messages.add("Hello Cars!");
 //        messages.add("I'm Spring MVC application");
 //        messages.add("5.2.0 version by sep'19 ");
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,8 +1,6 @@
 package web.service;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
-import web.config.WebConfig;
 import web.model.Car;
 
 import java.util.ArrayList;
@@ -10,19 +8,24 @@ import java.util.List;
 
 @Service
 public class CarServiceImp {
-//    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
-private static  List<Car> carList = null;
+    private static List<Car> carList = getNewCarList();
 
     public static List<Car> getCarList(int count) {
-        if (carList != null) {
-            return carList.subList(0, count);
+        if (carList != null && count > 0) {
+            return count > 5 ? carList : carList.subList(0, count);
         }
-        carList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            carList.add(new Car());
-            carList.get(i).setModel(String.valueOf(i));
-            carList.get(i).setColor(String.valueOf(i * 2));
-            carList.get(i).setHorsePower(i * 10);
+        return carList;
+    }
+
+    public static List<Car> getNewCarList() {
+        if (carList == null) {
+            carList = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                carList.add(new Car());
+                carList.get(i).setModel(String.valueOf(i));
+                carList.get(i).setColor(String.valueOf(i * 2));
+                carList.get(i).setHorsePower(i * 10);
+            }
         }
         return carList;
     }
