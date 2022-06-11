929cfb683989a04da102ed490a660464f0ae8662
vepolud <vepolud@gmail.com>
2022-06-11 22:13:30 +0300

    структура проекта готова

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarServiceImp;
 
 import java.util.List;
 
@@ -13,7 +11,7 @@ import java.util.List;
 public class CarController {
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "5") int count, ModelMap model) {
-        List<Car> messages = CarServiceImp.getCarList(count);
+        String messages = "Hello";
         model.addAttribute("messages", messages);
         return "cars";
     }
